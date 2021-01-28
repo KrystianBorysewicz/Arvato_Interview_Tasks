0b352a4f7ddd195f802d1647d7b2149605a092fb
Krystian Borysewicz <omnitored@gmail.com>
2021-01-28 18:17:10 +0100

    Add project files.

---
+++ ArvatoInterviewTasks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArvatoInterviewTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
